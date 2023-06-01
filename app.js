47750678e494d1c7da240bd16a9db37349e6da7a
filbert29 <fkhouwira70@gmail.com>
2023-06-02 01:51:52 +0700

    done final

--- app.js
+++ app.js
@@ -16,49 +16,111 @@ mailchimp.setConfig({
     server: "us12",
 });
 
-const run = async () => {
-    const response = await client.lists.getAllLists('8d0cce172a');
-    console.log(response);
+const createList = async () => {
+    try {
+        const response = await client.lists.createList({
+            name: "name",
+            permission_reminder: "permission_reminder",
+            email_type_option: true,
+            contact: {
+                company: "company",
+                address1: "address1",
+                city: "city",
+                country: "country",
+                state: "state",
+                zip: "12332"
+            },
+            campaign_defaults: {
+                from_name: "from_name",
+                from_email: "Beulah_Ryan@hotmail.com",
+                subject: "subject",
+                language: "language",
+            },
+        });
+        console.log(response);
+        res.redirect('/success.html')
+    } catch (err) {
+        console.log(err.response.body)
+    }
 }
 
-app.post('/test', async (req, res) => {
-    run();
 
-    // const { firstName, lastName, email } = req.body;
+app.post('/createList', async (req, res) => {
+    createList();
+})
 
-    // // Make sure fields are filled
-    // if (!firstName || !lastName || !email) {
-    //     res.redirect('/fail.html');
-    //     return;
+app.get('/getMembers', async (req, res) => {
+    const IdList = await client.lists.getAllLists();
+
+    const response = await client.lists.getListMembersInfo(IdList.lists[0].id);
+    console.log(response.members);
+    res.json(response.members);
+})
+
+app.get('/getIdList', async (req, res) => {
+    const response = await client.lists.getAllLists();
+    console.log(response.lists[0].id);
+})
+
+app.post('/addContact', async (req, res) => {
+    const IdList = await client.lists.getAllLists();
+
+    const { firstName, lastName, email } = req.body;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed',
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    console.log(data)
+
+    // try {
+    //     const response = await client.lists.batchListMembers(IdList.lists[0].id, data);
+    //     console.log(response);
+    // } catch (err) {
+    //     console.log(err.response.body)
     // }
+})
 
-    // // Construct req data
-    // const data = {
-    //     members: [
-    //         {
-    //             email_address: email,
-    //             status: 'subscribed',
-    //             merge_fields: {
-    //                 FNAME: firstName,
-    //                 LNAME: lastName
-    //             }
-    //         }
-    //     ]
-    // };
+app.post('/changeDataMember', async (req, res) => {
+    const IdList = await client.lists.getAllLists();
 
-    // const postData = JSON.stringify(data);
+    const { firstName, lastName, email, idMember } = req.body;
 
-    // fetch('https://usX.api.mailchimp.com/3.0/lists/8d0cce172a', {
-    //     method: 'POST',
-    //     headers: {
-    //         Authorization: 'auth 2da7ddd71707dfc2c6fbdc0b767684d8-us12'
-    //     },
-    //     body: postData
-    // })
-    // .then(res.statusCode === 200 ?
-    //     res.redirect('/success.html') :
-    //     res.redirect('/fail.html'))
-    // .catch(err => console.log(err))
+    try {
+        const response = await client.lists.updateListMember(IdList.lists[0].id, email, {
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName,
+            },
+        });
+        console.log(response);
+    } catch (err) {
+        console.log(err.response.body)
+    }
+})
+
+app.post('/unSubscribe', async (req, res) => {
+    const IdList = await client.lists.getAllLists();
+
+    const { email } = req.body;
+
+    try {
+        const response = await client.lists.updateListMember(IdList.lists[0].id, email, {
+            status: 'unsubscribed',
+        });
+        console.log(response);
+    } catch (err) {
+        console.log(err.response.body)
+    }
 })
 
 const PORT = process.env.PORT || 5000;
